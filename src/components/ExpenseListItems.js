f65a1e43f756de0cd77a1d504300345386cbb672
chekwas88 <princechekwas@gmail.com>
2018-08-12 10:39:50 +0100

    setup display format of expense list items

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from 'numeral';
 
-const ExpenseListItem = (props) => {
-    return(
-        <div> 
-            <Link to={`/edit/${props.id}`}>
-            <p>{props.description} </p>
-            </Link>
-            <p> {props.amount} - {props.createdAt}</p>
-            
-            
-        </div>
-    );
-}
-
- 
+const ExpenseListItem = props => {
+  return (
+    <div>
+      <Link to={`/edit/${props.id}`}>
+        <p>{props.description} </p>
+      </Link>
+      <p>
+        {numeral(props.amount).format('$0,0.00')} -
+        {moment(props.createdAt).format("MMMM Do, YYYY")}
+      </p>
+    </div>
+  );
+};
 
 export default ExpenseListItem;
